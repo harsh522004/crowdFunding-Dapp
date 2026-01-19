2a958f876980c3e81fca05570f8ea546d0859b07
Harsh Butani <harsh@elevator.global>
2026-01-19 15:48:23 +0530

    Step 2.1 Completed

---
+++ crowFunding-Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-9xl font-medium text-grey-500 text-center">
+        Hello world!
+      </div>
+    </>
+  );
+}
+
+export default App;
