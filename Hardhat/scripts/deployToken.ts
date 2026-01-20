d248eb33bf8b7539354219df7ebe6ff9fca83307
harsh522004 <harshbutani2004@gmail.com>
2026-01-20 21:05:36 +0530

    contract verfication using hardhat complete

---
+++ Hardhat/scripts/deployToken.ts
@@ -0,0 +1,33 @@
+import { network } from "hardhat";
+
+
+const {ethers , networkName } = await network.connect();
+async function displayInfo (){
+
+    // Network
+    console.log(`Connected to network: ${networkName}`);
+
+    // Deployer
+    const [deployer] = await ethers.getSigners();
+    console.log(`Deploying contracts with the account: ${deployer.address}`);
+
+    // balance of ETH
+    const balance = await ethers.provider.getBalance(deployer.address);
+    console.log(`Account balance: ${ethers.formatEther(balance)} ETH`);
+}
+
+async function main(){
+    await displayInfo();
+    const tokenContract = await ethers.deployContract("Karma");
+    await tokenContract.waitForDeployment();
+    const address : string = await tokenContract.getAddress();
+    console.log(`Deplyed Master contract at : ${address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("Deployment failed:");
+    console.error(error);
+    process.exit(1);
+  });
